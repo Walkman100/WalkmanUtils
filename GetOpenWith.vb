7fd4e2d513fcc300148df293d72868ae29401da6
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-11-17 15:03:10 +0200

    updated GetOpenWith, formatted AssemblyInfo.vb in VS

--- GetOpenWith.vb
+++ GetOpenWith.vb
@@ -4,33 +4,77 @@ Option Compare Binary
 Option Infer Off
 
 Imports System
+Imports System.Collections.Generic
 Imports System.Reflection
 
-<assembly: AssemblyTitle("GetOpenWith")>
-<assembly: AssemblyDescription("")>
-<assembly: AssemblyConfiguration("")>
-<assembly: AssemblyCompany("")>
-<assembly: AssemblyProduct("GetOpenWith")>
+<Assembly: AssemblyTitle("GetOpenWith")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GetOpenWith")>
 
 Module Program
-    Sub Main(args() As String)
-        If args.Length <> 1 Then
-            WriteUsage
-        ElseIf IO.File.Exists(args(0)) Then
-            Console.WriteLine(WalkmanLib.GetOpenWith(args(0)))
-        Else
-            Console.WriteLine("File """ & args(0) & """ not found!")
-            WriteUsage
-        End If
+    Function WriteUsage(Optional input As String = Nothing) As Boolean
+        Console.Error.WriteLine("Usage: " & GetProgramFileName() & " [OPTION] <FILE...>")
+        Console.Error.WriteLine("Get the path to the program specified to open a file, or ""Filetype not associated!"" if none is set " &
+                                "(WalkmanUtils - https://github.com/Walkman100/WalkmanUtils)" & Environment.NewLine)
+        WalkmanLib.EchoHelp(flagDict, input)
+        Environment.Exit(0)
+        Return True
+    End Function
+
+    Private flagDict As New Dictionary(Of String, WalkmanLib.FlagInfo) From {
+        {"help", New WalkmanLib.FlagInfo With {
+            .shortFlag = "h"c,
+            .description = "Show Help",
+            .hasArgs = True,
+            .optionalArgs = True,
+            .argsInfo = "[flag]",
+            .action = AddressOf WriteUsage
+        }},
+        {"files", New WalkmanLib.FlagInfo With {
+            .shortFlag = "f"c,
+            .description = "Print file path before associated program path",
+            .hasArgs = False,
+            .action = Function() DoAndReturn(Sub() printFilePaths = True)
+        }}
+    }
+
+    Private printFilePaths As Boolean = False
+
+    Private Sub WriteOpenWiths(files As List(Of String))
+        For Each file As String In files
+            If IO.File.Exists(file) Then
+                file = IO.Path.GetFullPath(file)
+                If printFilePaths Then Console.Write(file & ": ")
+
+                Try
+                    Dim openWith As String = WalkmanLib.GetOpenWith(file)
+                    Console.WriteLine(openWith)
+                Catch ex As Exception
+                    If Console.IsOutputRedirected Then Console.WriteLine("Error")
+                    Console.Error.WriteLine("Exception getting associated program: " & ex.Message)
+                End Try
+            Else
+                If Console.IsOutputRedirected Then
+                    If printFilePaths Then Console.Write(file & ": ")
+                    Console.WriteLine("File not found")
+                End If
+
+                Console.Error.WriteLine("File """ & file & """ not found!")
+            End If
+        Next
     End Sub
-    
-    Sub WriteUsage()
-        Dim flags As String = " <path>" & Environment.NewLine & "Get the path to the program specified to open a file, or ""Filetype not associated!"" if none is set (WalkmanUtils - https://github.com/Walkman100/WalkmanUtils)"
-        Dim programPath As String = System.Reflection.Assembly.GetExecutingAssembly().CodeBase
-        Dim programFile As String = programPath.Substring(programPath.LastIndexOf("/") +1)
-        If My.Computer.Info.OSPlatform = "Unix" Then
-            programFile = "mono " & programFile
+
+    Sub Main(args() As String)
+        Dim res As WalkmanLib.ResultInfo = WalkmanLib.ProcessArgs(args, flagDict, True)
+
+        If res.gotError Then
+            ExitE(res.errorInfo)
+        ElseIf res.extraParams.Count < 1 Then
+            WriteUsage()
+        Else
+            WriteOpenWiths(res.extraParams)
         End If
-        Console.WriteLine("Usage: " & programFile & flags)
     End Sub
 End Module
--- Properties/AssemblyInfo.vb
+++ Properties/AssemblyInfo.vb
@@ -7,16 +7,16 @@ Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 ' Information about this assembly is defined by the following attributes.
-<assembly: AssemblyCopyright("FOSS Walkman 2020")>
-<assembly: AssemblyTrademark("")>
-<assembly: AssemblyCulture("")>
+<Assembly: AssemblyCopyright("FOSS Walkman 2020")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
 
 ' This sets the default COM visibility of types in the assembly to invisible.
 ' If you need to expose a type to COM, use <ComVisible(true)> on that type.
-<assembly: ComVisible(False)>
+<Assembly: ComVisible(False)>
 
 ' The assembly version has following format :
 ' Major.Minor.Build.Revision
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
-<assembly: AssemblyVersion("1.0.*")>
+<Assembly: AssemblyVersion("1.0.*")>
