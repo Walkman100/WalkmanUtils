00263e7ecfa02e8e8af1b3c1b00c8edd4ce6b883
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-12-16 14:31:11 +0200

    Updated WalkmanLib, added WinProperties code with using new WalkmanLib.WaitForWindowByThread method

--- WinProperties.vb
+++ WinProperties.vb
@@ -4,57 +4,107 @@ Option Compare Binary
 Option Infer Off
 
 Imports System
+Imports System.Collections.Generic
+Imports System.Linq
 Imports System.Reflection
+Imports System.Threading
 Imports System.Windows.Forms
-Imports Microsoft.VisualBasic
 
-<assembly: AssemblyTitle("WinProperties")>
-<assembly: AssemblyDescription("")>
-<assembly: AssemblyConfiguration("")>
-<assembly: AssemblyCompany("")>
-<assembly: AssemblyProduct("WinProperties")>
+<Assembly: AssemblyTitle("WinProperties")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WinProperties")>
 
 Class WinProperties
-    Inherits System.Windows.Forms.Form
-    Public Sub New()
-        Dim args As String() = System.Environment.GetCommandLineArgs()
-        If args.Length <> 2 Then
-            Application.EnableVisualStyles()
-            MsgBox(GetUsage(), MsgBoxStyle.Information, "Command line arguments incorrect")
-        ElseIf IO.File.Exists(args(1)) Or IO.Directory.Exists(args(1)) Then
-            Application.EnableVisualStyles()
-            If WalkmanLib.ShowProperties(args(1)) Then
-                System.Threading.Thread.Sleep(2000)
-                'FIXME: properties window only shows for a second
+    Inherits Form
+
+    Function GetUsage(Optional input As String = Nothing) As String
+        Dim usageText As String = "Usage: " & GetProgramFileName() & " <FILE...>" & Environment.NewLine
+        usageText &= "Opens the Windows properties window for a path" & WalkmanUtilsText & Environment.NewLine & Environment.NewLine
+
+        Using sw As New IO.StringWriter
+            WalkmanLib.EchoHelp(flagDict, input, sw)
+            usageText &= sw.ToString()
+        End Using
+
+        Return usageText
+    End Function
+
+    Function ShowUsage(Optional input As String = Nothing) As Boolean
+        MessageBox.Show(GetUsage(input), "Program Usage", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Environment.Exit(0)
+        Return True
+    End Function
+
+    Private flagDict As New Dictionary(Of String, WalkmanLib.FlagInfo) From {
+        {"help", New WalkmanLib.FlagInfo With {
+            .shortFlag = "h"c,
+            .description = "Show Help",
+            .hasArgs = True,
+            .optionalArgs = True,
+            .argsInfo = "[flag]",
+            .action = AddressOf ShowUsage
+        }},
+        {"tab", New WalkmanLib.FlagInfo With {
+            .shortFlag = "t"c,
+            .description = "Open the properties window on a specified tab. Tab name is Language-specific.",
+            .hasArgs = True,
+            .argsInfo = "<tab name>",
+            .action = Function(value As String) DoAndReturn(Sub() tabToShow = value)
+        }}
+    }
+
+    Private tabToShow As String = Nothing
+
+    Private Sub ShowProperties(paths As List(Of String))
+        Dim waitThreads As New List(Of Tasks.Task)
+
+        For Each path As String In paths
+            If IO.File.Exists(path) Or IO.Directory.Exists(path) Then
+                If WalkmanLib.ShowProperties(path, tabToShow) Then
+                    Dim itemName As String = IO.Path.GetFileName(path)
+
+                    waitThreads.Add(Tasks.Task.Run(Sub()
+                                                       Thread.Sleep(600)
+                                                       MessageBox.Show("waiting for " & itemName & " started")
+                                                       WalkmanLib.WaitForWindowByThread(itemName & " Properties", "#32770")
+                                                       MessageBox.Show("waiting for " & itemName & " finished")
+                                                   End Sub))
+                Else
+                    MessageBox.Show("There was an error opening the properties window for """ & path & """!", "WinProperties", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                End If
             Else
-                MsgBox("There was an error opening the properties window for """ & args(1) & """!")
+                MessageBox.Show("File or directory """ & path & """ not found!" & Environment.NewLine & Environment.NewLine & GetUsage(), "Path not found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             End If
+        Next
+
+        Try
+            Tasks.Task.WaitAll(waitThreads.ToArray())
+        Catch ex As AggregateException
+            MessageBox.Show(ex.InnerException.ToString(), "Error waiting for one or more properties windows!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), "Error waiting for one or more properties windows!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Public Sub New()
+        Dim args As String() = Environment.GetCommandLineArgs().Skip(1).ToArray()
+        Dim res As WalkmanLib.ResultInfo = WalkmanLib.ProcessArgs(args, flagDict, True)
+
+        If res.gotError Then
+            MessageBox.Show(res.errorInfo, "Error processing arguments", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf res.extraParams.Count < 1 Then
+            ShowUsage()
         Else
-            Application.EnableVisualStyles()
-            MsgBox("File or directory """ & args(1) & """ not found!" & Environment.NewLine & Environment.NewLine & GetUsage(), MsgBoxStyle.Exclamation, "Path not found")
+            ShowProperties(res.extraParams)
         End If
-        
+
         Do Until 0 <> 0
-            Application.Exit
+            Application.Exit()
             End
         Loop
     End Sub
-    
-    Function GetUsage() As String
-        Dim flags As String = " <path>" & Environment.NewLine & "Opens the Windows properties window for a path"
-        flags &= Environment.NewLine & "WalkmanUtils - https://github.com/Walkman100/WalkmanUtils"
-        Dim programPath As String = System.Reflection.Assembly.GetExecutingAssembly().CodeBase
-        Dim programFile As String = programPath.Substring(programPath.LastIndexOf("/") +1)
-        If My.Computer.Info.OSPlatform = "Unix" Then
-            programFile = "mono " & programFile
-        End If
-        Return "Usage: " & programFile & flags
-        
-        ''' <summary>Opens the Windows properties window for a path.</summary>
-        ''' <param name="path">The path to show the window for.</param>
-        ''' <param name="tab">Optional tab to open to. Beware, this name is culture-specific!</param>
-        ''' <returns>Whether the properties window was shown successfully or not.</returns>
-    End Function
 End Class
 
 Namespace My
